14c83e9374cefdd0d9fdd0550eed8ce324943aca
Gadzhi Gadzhiev <resure@yandex-team.ru>
2014-11-08 14:34:50 +0300

    Lab #3

---
+++ third/client/lab.js
@@ -0,0 +1,193 @@
+function pretty(input) {
+    return JSON.stringify(input, null, '\t');
+}
+
+function pp(input) {
+    console.log(pretty(input));
+}
+
+function output(input) {
+    $('.output').append('<p>' + input + '</p>');
+}
+
+function copy(input) {
+    return JSON.parse(JSON.stringify(input));
+}
+
+function printMatr(input) {
+    var result = '    ',
+        N = input.length;
+
+    for (var i = 1; i <= N; i++) {
+        result += '<span style="color:#999;display:inline-block;transform:rotate(-45deg);padding-bottom:4px">' + i + '</span> ';
+        if (i < 10) {
+            result += ' ';
+        }
+    }
+
+    result += '<br>'
+
+    input.forEach(function (row, i) {
+        result += '<span style="color:#999">' + (i+1) + '</span>  ';
+        if (i < 9) {
+            result += ' ';
+        }
+        row.forEach(function (el) {
+            result += el + ' ';
+            if (el < 10 || el == 'T' || el == 'F') {
+                result += ' ';
+            }
+        });
+        result += '<br>';
+    });
+
+    output(result);
+}
+
+
+function compute(tree) {
+    var N = Object.keys(tree).length;
+    var matr = [];
+
+    for (var i = 1; i <= N; i++) {
+        var current = tree[i],
+            row = [],
+            conditionState = true;
+
+        for (var j = 0; j < N; j++) {
+            if (current[j + 1]) {
+                if (current.condition) {
+                    row[j] = conditionState ? 'T' : 'F';
+                    conditionState = false;
+                } else {
+                    row[j] = 1;
+                }
+            } else {
+                row[j] = 0;
+            }
+        }
+
+        matr.push(row);
+    }
+
+    var values = [];
+    for (var i = 1; i <= N; i++) {
+        values.push(tree[i].value);
+    }
+    matr.push(values);
+    matr = _.zip.apply(_, matr);
+    
+
+    
+
+    printMatr(matr);
+}
+
+
+
+
+
+
+
+
+
+$(function () {
+
+    var tree = {
+        1: {
+            4: 2,
+            value: 2,
+        },
+        2: {
+            4: 2,
+            5: 1,
+            value: 4,
+        },
+        3: {
+            5: 1,
+            10: 3,
+            value: 3,
+        },
+        4: {
+            6: 4,
+            7: 5,
+            condition: [4.1, 4.2],
+            value: 2,
+        },
+        5: {
+            8: 7,
+            9: 2,
+            value: 1,
+        },
+        6: {
+            11: 4,
+            value: 4,
+        },
+        7: {
+            12: 6,
+            value: 5,
+        },
+        8: {
+            12: 6,
+            value: 7,
+        },
+        9: {
+            13: 7,
+            value: 2,
+        },
+        10: {
+            14: 8,
+            15: 9,
+            condition: [10.1, 10.2],
+            value: 3,
+        },
+        11: {
+            value: 4,
+        },
+        12: {
+            value: 6,
+        },
+        13: {
+            16: 10,
+            value: 7,
+        },
+        14: {
+            16: 10,
+            value: 8,
+        },
+        15: {
+            value: 9,
+        },
+        16: {
+            17: 6,
+            18: 5,
+            condition: [16.1, 16.2],
+            value: 10,
+        },
+        17: {
+            value: 6,
+        },
+        18: {
+            value: 5,
+        },
+    };
+
+
+    compute(tree);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
